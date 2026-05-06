455f967591cfe556df8af072983f82ec8145df7f
Guilherme P. Andraz <guilherme.andraz@outlook.com>
2026-05-06 14:01:01 -0300

    Normalize input for nome and email fields

--- js/clientes.js
+++ js/clientes.js
@@ -1,8 +1,9 @@
+// Banco de dados
 const ref = db.ref("clientes");
 
 $("#salvar").click(function () {
-    let nome = $("#nome").val();
-    let email = $("#email").val();
+    let nome = $("#nome").val().toUpperCase();
+    let email = $("#email").val().toLowerCase();
 
     if (nome === "" || email === "") {
         alert("Preencha todos os campos!");
@@ -14,10 +15,50 @@ $("#salvar").click(function () {
         type: "cliente",
         email: email
     });
+
+    // Tabela no site
+
+    ref.on("value", dados_tabela => {
+        $("#lista").empty();
+
+        $("#lista").append(`
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th colspan="2">Opções</th>
+        </tr>`)
+
+        dados_tabela.forEach(registro => {
+            let reg = registro.val();
+            let id = registro.key;
+
+            $("#lista").append(`
+            <tr>
+                <td>${id}</td>
+                <td>${reg.nome}</td>
+                <td>${reg.email}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi-trash"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+        });
+    });
+
+    // Limpar os campos
+
     function limpar() {
         $("#nome").val("");
         $("#email").val("");
     }
-    limpar();
-    alert("Cliente salvo com sucesso!");
+    alert('Cliente Cadastrado com Sucesso!')
+    limpar()
 })
