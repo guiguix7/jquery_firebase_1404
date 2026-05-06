483057bcfb6e754d196afc1d1f6560058ce66c82
Guilherme P. Andraz <guilherme.andraz@outlook.com>
2026-05-06 14:01:35 -0300

    Normalize input fields for fornecedor data

--- js/fornecedores.js
+++ js/fornecedores.js
@@ -1,10 +1,11 @@
+// Banco de dados
 const ref = db.ref("fornecedores");
 
 $("#salvar").click(function () {
-    let nome = $("#nome").val();
-    let email = $("#email").val();
+    let nome = $("#nome").val().toUpperCase();
+    let email = $("#email").val().toLowerCase();
     let cnpj = $("#CNPJ").val();
-    let estado = $("input[name='estado']:checked").val();
+    let estado = $("input[name='estado']:checked").val().toUpperCase();
 
     if (nome === "" || email === "" || cnpj === "" || estado === "") {
         alert("Preencha todos os campos!");
@@ -24,6 +25,48 @@ $("#salvar").click(function () {
         $("#CNPJ").val("");
         $("input[name='estado']").prop("checked", false);
     }
+
+    ref.on("value", dados_tabela => {
+        $("#lista").empty();
+
+        $("#lista").append(`
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>CNPJ</th>
+            <th>Estado</th>
+            <th colspan="2">Opções</th>
+        </tr>`)
+
+        dados_tabela.forEach(registro => {
+            let reg = registro.val();
+            let id = registro.key;
+
+            $("#lista").append(`
+            <tr>
+                <td>${id}</td>
+                <td>${reg.nome}</td>
+                <td>${reg.email}</td>
+                <td>${reg.cnpj}</td>
+                <td>${reg.estado}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi-trash"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+        });
+    });
+
+    // Limpar os campos
+
     limpar();
     alert("Fornecedor salvo com sucesso!");
 })
