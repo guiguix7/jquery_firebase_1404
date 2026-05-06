6243095f53a215e01bd48a2df90173563349fd0e
Guilherme P. Andraz <guilherme.andraz@outlook.com>
2026-05-06 14:00:38 -0300

    Convert category name to uppercase on save
    Convert the category name to uppercase before saving.

--- js/categoria.js
+++ js/categoria.js
@@ -1,7 +1,8 @@
+// Banco de dados
 const ref = db.ref("categorias");
 
 $("#salvar").click(function () {
-    let nome = $("#nome").val();
+    let nome = $("#nome").val().toUpperCase();
     let text = $("#info").val();
 
     if (nome === "" || text === "") {
@@ -18,6 +19,46 @@ $("#salvar").click(function () {
         $("#nome").val("");
         $("#info").val("");
     }
+
+    // Tabela no site
+
+    ref.on("value", dados_tabela => {
+        $("#lista").empty();
+
+        $("#lista").append(`
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Informações</th>
+            <th colspan="2">Opções</th>
+        </tr>`)
+
+        dados_tabela.forEach(registro => {
+            let reg = registro.val();
+            let id = registro.key;
+
+            $("#lista").append(`
+            <tr>
+                <td>${id}</td>
+                <td>${reg.nome}</td>
+                <td>${reg.info}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi-trash"></i>
+                    </button>
+                </td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+        `);
+        });
+    });
+
+    // Limpar os campos
+
     limpar();
     alert("Categoria salva com sucesso!");
 })
