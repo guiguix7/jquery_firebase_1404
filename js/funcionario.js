2bdc0c4b3ba6520e4425fb3097e8279db93a50ab
X <X@x.com>
2026-05-20 15:14:12 -0300

    Adicionar página de Funcionários e atualizar navegação nos arquivos HTML

---
+++ js/funcionario.js
@@ -0,0 +1,92 @@
+// Banco de dados
+const ref = db.ref("funcionarios");
+
+let idcapturado = null
+
+$("#cancelar").hide()
+
+function limpar() {
+    $("#nome").val("");
+    $("#email").val("");
+    $("#cargo").val("");
+}
+
+$("#salvar").click(function () {
+    let nome = $("#nome").val().toUpperCase();
+    let email = $("#email").val().toLowerCase();
+    let cargo = $("#cargo").val();
+
+    if (nome === "" || email === "" || cargo === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    if (idcapturado) { // Atualizar
+        ref.child(idcapturado).update({ nome, email, cargo })
+        idcapturado = null
+        $("#salvar").text("Salvar")
+        $("#salvar").removeClass("btn-success").addClass("btn-primary")
+        $("#cancelar").hide()
+        $("#status").text("")
+        alert("Funcionário atualizado com sucesso!");
+    } else {
+        ref.push({
+            nome: nome,
+            email: email,
+            cargo: cargo,
+            type: "funcionario"
+        });
+        alert("Funcionário salvo com sucesso!");
+    }
+
+    limpar();
+})
+
+ref.on("value", dados_tabela => {
+    $("#lista").empty();
+
+    $("#lista").append(`
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Cargo</th>
+        <th colspan="2">Opções</th>
+    </tr>`)
+
+    dados_tabela.forEach(registro => {
+        let reg = registro.val();
+        let id = registro.key;
+
+        $("#lista").append(`
+        <tr>
+            <td>${id}</td>
+            <td>${reg.nome}</td>
+            <td>${reg.email}</td>
+            <td>${reg.cargo}</td>
+            <td>
+                <button class="btn btn-danger btn-sm" onclick="deletar('${id}')">
+                    <i class="bi-trash"></i>
+                </button>
+            </td>
+            <td>
+                <button class="btn btn-warning btn-sm" onclick="editar('${id}', '${reg.nome}', '${reg.email}', '${reg.cargo}')">
+                    <i class="bi bi-pencil"></i>
+                </button>
+            </td>
+        </tr>
+    `);
+    });
+});
+
+function editar(id, nome, email, cargo) {
+    $('#nome').val(nome)
+    $('#email').val(email)
+    $('#cargo').val(cargo)
+
+    idcapturado = id
+
+    $('#salvar').text('Atualizar').removeClass('btn-primary').addClass('btn-success')
+    $("#cancelar").show()
+    $("#status").text("Editando registro...")
+}
